c846a120b7c2c2392c9744e0cb61df9230c026e1
Bernardozomer <bernardobarzottoz@gmail.com>
2022-10-31 11:32:27 -0300

    feat: parse instructions

--- main.hs
+++ main.hs
@@ -24,3 +24,15 @@ data Reg = Reg {
 	regMDR :: Ptr,
 	regPC :: Ptr
 } deriving (Show)
+
+-- Parse an instruction encoded in two Word8's.
+parseInstr :: (Word8, Word8) -> Instr
+parseInstr  (2, y) = Lod (Ptr y)
+parseInstr  (4, y) = Sto (Ptr y)
+parseInstr  (6, y) = Jmp (Ptr y)
+parseInstr  (8, y) = Jmz (Ptr y)
+parseInstr (10, y) = Cpe (Ptr y)
+parseInstr (14, y) = Add (Ptr y)
+parseInstr (16, y) = Sub (Ptr y)
+parseInstr (18, y) = Nop
+parseInstr (20, y) = Hlt
