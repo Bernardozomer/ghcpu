7b77fe00e7736cc2aade2c46bd0fe9a8c3f44f0b
Bernardozomer <bernardobarzottoz@gmail.com>
2022-10-31 11:30:04 -0300

    feat: use record syntax for registers

--- main.hs
+++ main.hs
@@ -15,12 +15,12 @@ data Instr =
 newtype Ptr = Ptr Word8 deriving (Show)
 newtype Val = Val Word8 deriving (Show)
 
-data Reg =
-	  ACC
-	| EQZ
-	| IC
-	| IR
-	| MAR
-	| MDR
-	| PC
-	deriving (Show)
+data Reg = Reg {
+	regACC :: Val,
+	regEQZ :: Bool,
+	regIC :: Ptr,
+	regIR :: (Word8, Word8),
+	regMAR :: Ptr,
+	regMDR :: Ptr,
+	regPC :: Ptr
+} deriving (Show)
